fb0b18c32b341b99e4b65786bd27c01a8549fe93
Ian McLoughlin <mcloughlin.ian@gmail.com>
2015-10-01 22:08:44 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+###### Semantic Web and Linked Data - Exercise 4
+# Internet Movie Database API
+In this exercise we will build a HTTP API for an IMDB generated CSV file.
+
+## Exercises
+1.  
+
+## Advanced exercises
+1.
+
+## Note
+- The dataset came from [this](had.co.nz/data/movies/) source. There is a description of the fields available there.
---
+++ imdb.js
@@ -0,0 +1,34 @@
+// Import the fs module so that we can read in files.
+var fs = require('fs');
+// Import express to create and configure the HTTP server.
+var express = require('express');
+
+// Read in the text file and parse it for JSON.
+var data = JSON.parse(fs.readFileSync('ratings.json','utf8'));
+
+// Create a HTTP server app.
+var app = express();
+
+// When a user goes to /, return a small help string.
+app.get('/', function(req, res) {
+  res.send('Try http://127.0.0.1:8000/movie/id/123 or http://127.0.0.1:8000/movie/year/1900.');
+});
+
+// Send back the JSON for movie i at /movie/id/i.
+app.get('/movie/id/:id', function(req, res) {
+  res.json(data[req.params.id]);
+});
+
+// Send back the JSON for the movies from year i at /movie/year/i.
+app.get('/movie/year/:year', function(req, res) {
+  movies = [];
+  
+  for (var i = 0; i < data.length; i++)
+    if (data[i].year == req.params.year)
+      movies.push(data[i]);
+  
+  res.json(movies);
+});
+
+// Start the server.
+var server = app.listen(8000);
